17e93ab75c2a9034a7f0d99c5fbf275a0700f06d
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2022-11-22 16:10:20 +0100

    init commit

---
+++ src/main/java/com/polytech/fast_seasonal_gateway/FastSeasonalGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.fast_seasonal_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastSeasonalGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastSeasonalGatewayApplication.class, args);
+    }
+
+}
